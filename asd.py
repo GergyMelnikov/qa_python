1cde7430024a0e116318dd98504e4ef6d664da25
georgiy <georgiy.melnikov@webasyst.com>
2024-04-02 17:17:25 +0300

    Add parametrized test_set_book_genre_true

---
+++ asd.py
@@ -0,0 +1,8 @@
+a = {'фыв':'frf', 213:132}
+b= 'фыв'
+c = 'frf'
+
+for b,c in a.items():
+    b='фыв'
+    c='frf'
+    print(b,c)   
--- tests.py
+++ tests.py
@@ -25,9 +25,24 @@ class TestBooksCollector:
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
     
     
-    def test_add_new_book_title_more_forty_characters_false(self):
+    def test_add_new_book_title_less_then_forty_characters_true(self):
         new_book = BooksCollector() 
         long_name = 'a' * 41
+        normal_name = 'a' * 40
         new_book.add_new_book(long_name)
+        new_book.add_new_book(normal_name)
 
-        assert long_name not in new_book.books_genre
+        assert long_name not in new_book.books_genre and normal_name in new_book.books_genre
+
+    @pytest.mark.parametrize('some_book, genre', [['Некоторая книга', 'Фантастика'],['Шина','Ужасы'],['Доктор гаджет','Детективы'],['Шестиструнный самурай', 'Мультфильмы'],['Токсичный мститель', 'Комедии']])
+    def test_set_book_genre_true(self, some_book, genre):
+        new_book = BooksCollector()
+        #some_book = 'Некоторая книга'
+        #genre = 'Фантастика'
+        new_book.add_new_book(some_book)
+        new_book.set_book_genre(some_book, genre)
+
+        assert new_book.books_genre.get(some_book) == genre
+
+        
+
