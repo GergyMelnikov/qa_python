f434e47f2763ce4ce5fab715535bacd4ae4f51d8
georgiy <georgiy.melnikov@webasyst.com>
2024-04-05 12:45:38 +0300

    Correct asserts with using methods

--- tests.py
+++ tests.py
@@ -16,10 +16,9 @@ class TestBooksCollector:
         # добавляем две книги
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
         # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_genre()) == 2 # было `collector.get_books_rating()`, наверное опечатка? Мешало - исправил на `.get_books_genre())`
+        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2        
+        assert len(collector.get_books_genre()) == 2
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()  
@@ -31,7 +30,7 @@ class TestBooksCollector:
         library.add_new_book(long_name)
         library.add_new_book(normal_name)
 
-        assert long_name not in library.books_genre and normal_name in library.books_genre
+        assert long_name not in library.get_books_genre() and normal_name in library.get_books_genre()
 
 
     @pytest.mark.parametrize('book_name, genre', 
@@ -47,7 +46,7 @@ class TestBooksCollector:
         library.add_new_book(book_name)
         library.set_book_genre(book_name, genre)
 
-        assert library.books_genre.get(book_name) == genre
+        assert library.get_book_genre(book_name) == genre
 
 
     @pytest.mark.parametrize('book_name, genre', 
@@ -62,10 +61,8 @@ class TestBooksCollector:
         library = BooksCollector()
         library.add_new_book(book_name)
         library.set_book_genre(book_name, genre)
-        
-        book_to_get = library.get_book_genre(book_name)
 
-        assert book_to_get == genre
+        assert library.get_book_genre(book_name) == genre
 
 
     def test_get_books_with_specific_genre_true(self):
@@ -79,13 +76,12 @@ class TestBooksCollector:
         for book_name, genre in books_list:
             library.add_new_book(book_name)
             library.set_book_genre(book_name, genre)
-        
         received_books = library.get_books_with_specific_genre('Детективы')
         expected_books = ['Кошмары аиста марабу', 'Пять бутылок водки']
-
+        
         assert received_books == expected_books
 
-    
+
     def test_get_books_genre_true(self):
         library = BooksCollector()
         books_list = [
@@ -93,17 +89,16 @@ class TestBooksCollector:
             ['Где? Сколько?', 'Детективы'],
             ['Коты трюкачи', 'Фантастика']
         ]
-        
         for book_name, genre in books_list:
             library.add_new_book(book_name)
             library.set_book_genre(book_name, genre)
-
         expected_library = {'Ужасные истории':'Ужасы','Где? Сколько?':'Детективы','Коты трюкачи':'Фантастика'}
         received_library = library.get_books_genre()
         
         assert expected_library == received_library                       
 
-    def test_get_books_gor_children_true(self):
+
+    def test_get_books_for_children_true(self):
         library = BooksCollector()
         books_list = [
             ['Ужасные истории', 'Ужасы'],
@@ -113,15 +108,13 @@ class TestBooksCollector:
             ['11111','Комедии'],
             ['Как мультфильм попал в книгу', '']
         ]
-        
         for book_name, genre in books_list:
             library.add_new_book(book_name)
             library.set_book_genre(book_name, genre)
-
         expected_books = ['Коты трюкачи', 'А книги про котов быают?','11111']
         received_books = library.get_books_for_children()
-
         assert expected_books == received_books
+
 
     def test_add_book_in_favorites_true(self):
         library = BooksCollector()
@@ -136,19 +129,17 @@ class TestBooksCollector:
             ['А книги про котов быают?', 'Мультфильмы'],
             ['Как мультфильм попал в книгу', '']
         ]
-        
         for book_name, genre in books_list:
             library.add_new_book(book_name)
             library.set_book_genre(book_name, genre)
-
         library.add_book_in_favorites(favorite_book_1)
         library.add_book_in_favorites(favorite_book_2)
-
         expected = [favorite_book_1, favorite_book_2]
         result = library.get_list_of_favorites_books()
-        
+       
         assert expected == result
 
+   
     def test_delete_book_in_favorites_true(self):
         library = BooksCollector()
         favorite_book_1 = 'Дзен'
@@ -162,23 +153,20 @@ class TestBooksCollector:
             ['А книги про котов быают?', 'Мультфильмы'],
             ['Как мультфильм попал в книгу', '']
         ]
-        
         for book_name, genre in books_list:
             library.add_new_book(book_name)
             library.set_book_genre(book_name, genre)
-
         library.add_book_in_favorites(favorite_book_1)
         library.add_book_in_favorites(favorite_book_2)
         library.delete_book_from_favorites(favorite_book_1)
-
         expected = [favorite_book_2]
         result = library.get_list_of_favorites_books()
-        
+      
         assert expected == result
 
+   
     def test_get_list_of_favorites_books_empty_list(self):
         lib = BooksCollector()
         expected = []
         result = lib.get_list_of_favorites_books()
-
         assert expected == result
